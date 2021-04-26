7817545d143c9d5b68ae1833de77e2c0b2906922
sam <sblan45@gmail.com>
2021-04-25 21:58:41 -0400

    adds a password generator with basic HTML, property bindings, event bindings, and interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  password = '';
+
+  onButtonClick() {
+    this.password = 'my password'
+  }
+
+  getPassword() {
+    return this.password;
+  }
+
 }
